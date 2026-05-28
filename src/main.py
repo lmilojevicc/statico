3fc4d9a152b8f6c725ea79ee82c6e16190ceb35d
Luka <104729643+lmilojevicc@users.noreply.github.com>
2026-05-28 13:38:33 +0200

    fix: basepath arg position

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from static import copy_contents
 def main():
     basepath = "/"
     if len(sys.argv) > 1:
-        basepath = sys.argv[0]
+        basepath = sys.argv[1]
 
     copy_contents("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
